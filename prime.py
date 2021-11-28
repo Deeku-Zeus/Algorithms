5ab51877fc67c268efe44dc190206d8504a8267a
Deeku-Zeus <deekuzeus21@gmail.com>
2021-11-29 00:51:19 +0530

    added changes

--- prime.py
+++ prime.py
@@ -2,6 +2,8 @@ import io
 import os
 
 def isPrime(n):
+    if n < 2:
+        return False
     for i in range(2,n):
         if n%i == 0:
             return False
@@ -10,11 +12,11 @@ def isPrime(n):
 def printPrimes(n,m):
     res=[]
     for i in range(n,m+1):
-        print("i i i="+str(i))
+        if i < 2:
+            continue
         f=True
         for j in range(2,i):
             f=True
-            print("j="+str(j))
             if i%j == 0:
                 f=False
                 break
@@ -25,12 +27,12 @@ while(True):
     try:
         op = int(input("Press 1 for checking if number is Prime.\nPress 2 for printing primes in a range [n m].\n"))
         if op != 1 and op != 2:
-            os.system("cls")
+            #os.system("cls")
             print("Choose Again !!")
             continue
         break
     except:
-        os.system("cls")
+        #os.system("cls")
         print("Choose Again !!")
 while(True):
     if op == 1 :
@@ -43,20 +45,22 @@ while(True):
                 print("{} is not a prime number.".format(n))
             break
         except:
-            os.system("cls")
+            #os.system("cls")
             print("Choose Again !!")
     if op == 2:
         try:
-            r=(input("Enter range n m :")).split(" ",1)
+            r=list((input("Enter range n m :")).split(" ",1))
+            r.sort()
             n=int(r[0])
             m=int(r[1])
             res = printPrimes(n,m)
             if not res:
-                print("{}-{} doesn't have any prime numbers.".format(n,m))
+                print("{} - {} doesn't have any prime numbers.".format(n,m))
             else:
-                print("Primes:\n")
+                print("Primes:")
                 print(*res,sep="\n")
+                print("range {} - {} has {} primes !!".format(n,m,len(res)))
             break
         except:
-            os.system("cls")
+            #os.system("cls")
             print("Choose Again !!")
